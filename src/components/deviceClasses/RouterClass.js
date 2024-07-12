d53154bc65792ab80c15566ad74349b95bfbcf8b
PatrickLeon24 <20211454@ALOE.ULIMA.EDU.PE>
2024-07-12 16:40:02 -0500

    aCTUALIZACION XD
    ola si

--- src/components/deviceClasses/RouterClass.js
+++ src/components/deviceClasses/RouterClass.js
@@ -1,6 +1,5 @@
 import InterfaceClass from "./Interface/InterfaceClass";
 import DeviceClass from "./DeviceClass";
-
 class RouterClass extends DeviceClass {
   constructor(nombre) {
     super(nombre);
@@ -10,26 +9,144 @@ class RouterClass extends DeviceClass {
       "GigabitEthernet0/2": new InterfaceClass("GigabitEthernet0/2"),
     };
     this.routingTable = {};
-    this.tablaRT={};
   }
 
   configurarInterface(nombre, ip, mascara) {
     if (nombre in this.interfaces) {
-      this.interfaces[nombre].configurar(ip, mascara);
+      this.interfaces[nombre].configurar(ip, mascara, null);
     } else {
       throw new Error(`Interface ${nombre} no existe`);
     }
   }
 
+  liberarInterface(nombre) {
+    if (nombre in this.interfaces) {
+      this.interfaces[nombre].liberar();
+    } else {
+      throw new Error(`Interface ${nombre} no existe`);
+    }
+  }
+
+  addVecino(interfaz, dispositivo, distancia = 1) {
+    if (interfaz in this.interfaces && !this.interfaces[interfaz].ocupada) {
+      super.addVecino(interfaz, dispositivo, distancia);
+      this.interfaces[interfaz].ocupada = true;
+    } else {
+      throw new Error(`Interface ${interfaz} no existe o ya está ocupada`);
+    }
+  }
+
+  agregarRutaEstatica(redDestino, mascara, nextHop) {
+    this.routingTable[redDestino] = {
+      mascara: mascara,
+      nextHop: nextHop,
+    };
+  }
+
+  buscarRuta(ipDestino) {
+    for (let red in this.routingTable) {
+      if (this.ipEnSubred(ipDestino, red, this.routingTable[red].mascara)) {
+        return this.routingTable[red].nextHop;
+      }
+    }
+    return null;
+  }
+
+  buscarDispositivo(origen, destinationIp) {
+    let response = [`Request timeout for ${destinationIp}`, false]
+    for (let interfaz of Object.values(this.interfaces)) {
+      if (interfaz.ip && interfaz.mascara) {
+        console.log("encontre redes", this.obtenerRed(interfaz.ip, interfaz.mascara))
+        if (interfaz.ip === destinationIp) {
+          return [`Reply from device ${this.nombre}: bytes=32 time<1ms TTL=128 `, true];
+        } else if (this.obtenerRed(interfaz.ip, interfaz.mascara) === destinationIp) {
+          return [`Reply from network ${destinationIp}: bytes=32 time<1ms TTL=128 `, true];
+        } else if (
+          this.ipEnSubred(
+            destinationIp,
+            this.obtenerRed(interfaz.ip, interfaz.mascara),
+            interfaz.mascara
+          )
+        ) {
+          let dispositivoConectado = this.vecinos[interfaz.nombre];
+          if (origen !== dispositivoConectado) {
+            const [respuesta, state] = dispositivoConectado.buscarDispositivo(this, destinationIp);
+            if (state === true) {
+              return [respuesta, state]
+            }
+          }
+        }
+      }
+    }
+
+    let nextHop = this.buscarRuta(destinationIp);
+    if (nextHop !== null) {
+      console.log(nextHop)
+      console.log('interfaz salida nombre', this.obtenerInterfazPorRed(nextHop))
+      let interfazSalida = this.obtenerInterfazPorRed(nextHop);
+      if (interfazSalida !== null) {
+        console.log(interfazSalida)
+        if (interfazSalida.ocupada === true) {
+          let dispositivoConectado = this.vecinos[interfazSalida.nombre];
+          if (dispositivoConectado !== null) {
+            console.log('dispositivo conectado', dispositivoConectado)
+            return dispositivoConectado.buscarDispositivo(this, destinationIp);
+          }
+        }
+      }
+    }
+    console.log('ya fue')
+    return response
+  }
+
+  obtenerInterfazPorRed(nextHop) {
+    for (let interfaz of Object.values(this.interfaces)) {
+      if (interfaz.ip !== null & interfaz.mascara !== null) {
+        if (this.ipEnSubred(nextHop, interfaz.ip, interfaz.mascara) === true) {
+          return interfaz;
+        }
+      }
+    }
+    return null;
+  }
+
+  obtenerRed(ip, mascara) {
+    let ipBin = this.ipToBinary(ip);
+    let redBin = this.ipToBinary(this.obtenerRedDesdeIpYMascara(ip, mascara));
+    return this.binaryToIp(redBin);
+  }
+
   ipEnSubred(ip, red, mascara) {
-    const ipBin = this.ipToBinary(ip);
-    const redBin = this.ipToBinary(red);
-    const mascaraBin = this.ipToBinary(mascara);
+    let ipBin = this.ipToBinary(ip);
+    let redBin = this.ipToBinary(red);
+    let mascaraBin = this.ipToBinary(mascara);
     return (ipBin & mascaraBin) === (redBin & mascaraBin);
   }
 
+
+
   ipToBinary(ip) {
-    return ip.split('.').reduce((acc, octet) => (acc << 8) | parseInt(octet, 10), 0);
+    let octetos = ip.split(".").map((number) => {
+      return parseInt(number, 10);
+    });
+
+    return (octetos[0] << 24) | (octetos[1] << 16) | (octetos[2] << 8) | octetos[3];
+  }
+
+
+  binaryToIp(binary) {
+    let octeto1 = (binary >>> 24) & 0xff;
+    let octeto2 = (binary >>> 16) & 0xff;
+    let octeto3 = (binary >>> 8) & 0xff;
+    let octeto4 = binary & 0xff;
+    return `${octeto1}.${octeto2}.${octeto3}.${octeto4}`;
+  }
+
+  obtenerRedDesdeIpYMascara(ip, mascara) {
+    let ipBin = this.ipToBinary(ip);
+    let mascaraBin = this.ipToBinary(mascara);
+    let redBin = ipBin & mascaraBin;
+    return this.binaryToIp(redBin);
   }
 
   dijkstra(distancias, visitados, distAcum, predecesor, predecesores) {
@@ -143,8 +260,6 @@ class RouterClass extends DeviceClass {
   tieneIpConfigurada(interfaz) {
     return this.interfaces[interfaz].ip !== null && this.interfaces[interfaz].mascara !== null;
   }
-
-
 
   getInfo() {
     let info = super.getInfo();
